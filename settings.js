b679025c9d238855f70d37815d1e9a14543cbcfd
Professor-Ashan <ashan@example.com>
2026-07-22 10:16:49 +0000

    Initial upload of extracted files

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/anls16.jpg',
+    ownerNumber: process.env.OWNER_NUMBER || '92302742882',
+    botName: 'SYED MINI BOT',
+    ownerName: 'SYED',
+    whatsappChannel: 'https://whatsapp.com/channel/0029VaGiJKfIiRoybPBMTy38',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
